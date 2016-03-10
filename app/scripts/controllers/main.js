621d9b3b6b2a2c4261febcd95b379ca41ce62c62
Brandon Hay <brandon.hay@blackbaud.com>
2016-03-10 13:18:27 -0500

    able to change states of test steps in batch

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,6 +9,8 @@
  */
 angular.module('otgQualityInsightsApp')
 .controller('MainCtrl', function ($scope) {
+  
+  // all our products for which we run smoke tests
   $scope.products = [
     {
       name: "Outcomes",
@@ -24,6 +26,13 @@ angular.module('otgQualityInsightsApp')
     }
   ];
   
+  // batch updates
+  $scope.selectedItems = [];
+  $scope.changeState = function(selectedItems, state) {
+    selectedItems.forEach( function(item) {
+      item.state = state;
+    });
+  };
   
   $scope.outcomes = {
     name: "Outcomes",
@@ -31,47 +40,47 @@ angular.module('otgQualityInsightsApp')
       {
         description: "Create and activate an Outcomes-enabled application form.",
         order: 1,
-        pass: false
+        state: ""
       },
       {
         description: "Access Outcomes application and submit it.",
         order: 2,
-        pass: false
+        state: ""
       },
       {
         description: "Consider the submitted application in Pending Applications.",
         order: 3,
-        pass: false
+        state: ""
       },
       {
         description: "Access the Request Record and approve the grant.",
         order: 4,
-        pass: false
+        state: ""
       },
       {
         description: "Create and publish a requirement for the grant.",
         order: 5,
-        pass: false
+        state: ""
       },
       {
         description: "Access and submit the requirement via Graceland.",
         order: 6,
-        pass: false
+        state: ""
       },
       {
         description: "Generate Progress Updates for the grant.",
         order: 7,
-        pass: false
+        state: ""
       },
       {
         description: "Submit a Progress Update for the grant.",
         order: 8,
-        pass: false
+        state: ""
       },
       {
         description: "Verify that the progress update displays in the Funder Dashboard.",
         order: 9,
-        pass: false
+        state: ""
       }
     ]
   };
